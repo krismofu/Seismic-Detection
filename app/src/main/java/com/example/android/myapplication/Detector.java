49cd43b3f333f6e69a4d242685b1daeaed377f38
marulitua <erwinmaruli@live.com>
2014-11-23 18:27:18 +0700

    add notification

--- app/src/main/java/com/example/android/myapplication/Detector.java
+++ app/src/main/java/com/example/android/myapplication/Detector.java
@@ -1,11 +1,16 @@
 package com.example.android.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
 import android.content.Context;
 import android.hardware.SensorEvent;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -97,6 +102,8 @@ public class Detector implements SensorEventListener {
                         Log.i("ALERT", alert);
                         if(alert == "true") {
                             Toast.makeText(context, "Terjadi Gempa", Toast.LENGTH_SHORT).show();
+
+                            showNotification();
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -117,5 +124,33 @@ public class Detector implements SensorEventListener {
 
     public void turnOff() {
         mSensorManager.unregisterListener(this);
+    }
+
+    public void showNotification() {
+        //define soune URI, the sound to be played when there's a notification
+        //Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri soundUri = Uri.parse("android.resource://com.example.android.myapplication/"+R.raw.alarm_collision);
+
+        // intent triggered, you can add other intent for other actions
+        //Intent intent = new Intent(MyActivity.this, MyActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MyActivity.class), 0);
+
+        // this is it, we'll build the notification!
+        // in the addAction method, if you don't want any icon, just set the first param to 0
+        Notification mNotification = new Notification.Builder(context)
+                .setContentTitle("ALERT!")
+                .setContentText("Terjadi Gempa!")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(pIntent)
+                .setSound(soundUri).addAction(R.drawable.ic_launcher, "View", pIntent)
+                        //.addAction(0, "Remind", pIntent)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // If you want to hide the notification after it was selected, do the code below
+        // myNotification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(0, mNotification);
     }
 }
--- app/src/main/java/com/example/android/myapplication/MyService.java
+++ app/src/main/java/com/example/android/myapplication/MyService.java
@@ -1,7 +1,11 @@
 package com.example.android.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
 import android.widget.Toast;
 
@@ -21,6 +25,8 @@ public class MyService extends Service {
 
         // Let it continue running until it is stopped.
         Toast.makeText(this, "Service Started Host", Toast.LENGTH_SHORT).show();
+
+        //showNotification();
         return START_STICKY;
     }
 
@@ -34,5 +40,33 @@ public class MyService extends Service {
         System.gc();
 
         Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showNotification() {
+        //define soune URI, the sound to be played when there's a notification
+        //Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri soundUri = Uri.parse("android.resource://com.example.android.myapplication/"+R.raw.alarm_collision);
+
+        // intent triggered, you can add other intent for other actions
+        //Intent intent = new Intent(MyActivity.this, MyActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MyActivity.class), 0);
+
+        // this is it, we'll build the notification!
+        // in the addAction method, if you don't want any icon, just set the first param to 0
+        Notification mNotification = new Notification.Builder(this)
+                .setContentTitle("ALERT!")
+                .setContentText("Terjadi Gempa!")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(pIntent)
+                .setSound(soundUri).addAction(R.drawable.ic_launcher, "View", pIntent)
+                        //.addAction(0, "Remind", pIntent)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // If you want to hide the notification after it was selected, do the code below
+        // myNotification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(0, mNotification);
     }
 }
